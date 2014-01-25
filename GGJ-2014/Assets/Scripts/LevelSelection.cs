e0386acb4c6a339cc7b8fc38b8ba2da7027b26ce
NinjaGal <ajenika@gmail.com>
2014-01-25 18:39:46 +0800

    level selection

---
+++ GGJ-2014/Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnClicked5x5(){
+
+	}
+
+	private void OnClicked6x6(){
+
+	}
+
+	private void OnClicked7x7(){
+
+	}
+
+	private void OnClicked8x8(){
+
+	}
+
+	private void OnClicked9x9(){
+
+	}
+}
