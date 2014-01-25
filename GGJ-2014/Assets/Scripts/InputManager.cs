67118c60dcb5742429384f73e08cb08e539e67f6
Richard Locsin <goofydelinquent@gmail.com>
2014-01-25 08:52:18 +0800

    Added mouse button interaction

---
+++ GGJ-2014/Assets/Scripts/InputManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	public string m_candyLayer = "Candies";
+
+	void Start () {
+	
+	}
+
+	void Update () {
+
+		// 0 - Left, 1 - Right, 2 - Middle 
+		if( Input.GetMouseButtonUp( 0 ) ) {
+
+			int cLayer = 1 << LayerMask.NameToLayer( m_candyLayer );
+
+			Vector3 position = Input.mousePosition;
+
+			Vector3 worldPoint = Camera.main.ScreenToWorldPoint( position );
+			Collider2D collider = Physics2D.OverlapPoint( new Vector2( worldPoint.x, worldPoint.y ), cLayer );
+			if ( collider != null  )
+			{
+				Debug.Log( "Collided: " + collider.name );
+				Candy c = collider.GetComponent<Candy>();
+				if ( c != null ) {
+
+
+
+				}
+			}
+
+			// If using 3D collider
+			/*
+			Ray ray = camera.ScreenPointToRay( position );
+			Debug.DrawRay( ray.origin, ray.direction * 50, Color.green, 2f );
+
+			RaycastHit hit;
+			if ( Physics.Raycast( ray, out hit, Mathf.Infinity, cLayer ) ) {
+
+				Debug.Log( "HIT: " + hit.collider.name );
+
+			}*/
+
+		}
+	
+	}
+}
