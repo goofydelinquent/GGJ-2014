c0faebab9fd5574ec6b7301839bbc37510efd6fd
NinjaGal <ajenika@gmail.com>
2014-01-26 12:52:57 +0800

    credtis screen

--- GGJ-2014/Assets/Scripts/CreditsCreen.cs
+++ GGJ-2014/Assets/Scripts/CreditsCreen.cs
@@ -11,9 +11,18 @@ public class CreditsCreen : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-		if( Input.GetMouseButtonDown(0) )
+		if( Input.GetMouseButtonDown(0))
 		{
-			Application.LoadLevel("TitleScreen");
+			Vector3 worldPoint = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+			int layer = 1 << LayerMask.NameToLayer( "Default" );
+			Vector2 point = new Vector2( worldPoint.x, worldPoint.y );
+			Collider2D collider = Physics2D.OverlapPoint(point, layer );
+			if ( collider != null  ){
+
+			
+					Application.LoadLevel("TitleScreen");
+
+			}
 		}
 
 
